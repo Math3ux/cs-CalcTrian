912c774c82ca4e9cae0c80cb57e422fc2a86c4c0
zLuskx <matheusleidow@gmail.com>
2022-01-30 19:02:05 -0300

    commit

---
+++ CalcTrian/Triangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CalcTrian {
+    class Triangle {
+        public double A;
+        public double B;
+        public double C;
+
+        public double Area() {
+            double p = (A + B + C) / 2.0;
+            double raiz = Math.Sqrt(p * (A - p) * (B - p) * (C - p));
+            return raiz;
+        }
+
+    }
+}
